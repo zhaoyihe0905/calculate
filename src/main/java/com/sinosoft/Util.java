7751b2da75ea37d4d2981934363cab1b409e2d18
zhaoyihe <939694105@qq.com>
2020-03-31 18:27:55 +0800

    疫情有效期判断修改

--- src/main/java/com/sinosoft/Util.java
+++ src/main/java/com/sinosoft/Util.java
@@ -153,7 +153,7 @@ public class Util {
         //获取疫情起止日期内的有效天数
         for(Timestamp t:timestampSet){
             long l = t.getTime()/1000;
-            if(NCPStartDate/1000<=l&&l<=NCPEndDate/1000){
+            if(NCPStartDate/1000<=l&&l<NCPEndDate/1000){
                 count=count+1;
             }
         }
